5e6b6db9678008f643c617283ed888f95de3d9ea
Austin Stewart <hello@austinthedeveloper.com>
2015-10-18 15:48:14 -0500

    Cleanup and kiosk call

--- bcycle-service.js
+++ bcycle-service.js
@@ -1,13 +1,9 @@
 angular.module('bcycle', []);
 angular.module('bcycle')
  .factory('bcycle', function ($http, $q) {
-    var bCycleUrl = '../data/cities.json',
-      bCycleKiosks = '../data/kiosks.json',
-      bCycleTest = '../data/test.json',
-      bCycleApi = YOURKEYHERE,
-      useLocal = false;
+    var bCycleApi = YOURKEYHERE;
 
-    if(!useLocal && bCycleApi.length >= 32) {
+    if(bCycleApi.length >= 32) {
       bCycleUrl = 'https://publicapi.bcycle.com/api/1.0/ListPrograms/';
       bCycleKiosks = 'https://publicapi.bcycle.com/api/1.0/ListProgramKiosks/';
     }
@@ -18,17 +14,29 @@ angular.module('bcycle')
     }; 
 
     return {
-        getCities: function() {
-            var delay = $q.defer();
-            $http.get(bCycleUrl, headerOptions)
-              .success(function(data) {
-                delay.resolve(data);
-              })
-              .error(function(data) {
-                delay.resolve(data);
-              });
+			getCities: function() {
+        var delay = $q.defer();
+        $http.get(bCycleUrl, headerOptions)
+          .success(function(data) {
+            delay.resolve(data);
+          })
+          .error(function(data) {
+            delay.resolve(data);
+          });
 
-            return delay.promise;
-        }
+        return delay.promise;
+      },
+      getKiosks: function(city) {
+        var delay = $q.defer();
+        $http.get(bCycleKiosks + city, headerOptions)
+          .success(function(data) {
+            delay.resolve(data);
+          })
+          .error(function(data) {
+            delay.resolve(data);
+          });
+
+        return delay.promise;
+      }
     };
   });
